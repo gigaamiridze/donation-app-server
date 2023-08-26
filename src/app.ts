538fd12382e0609e8b628f4509dbda7def2a0bd9
Giga Amiridze <gigaamiridze@gmail.com>
2023-08-26 23:12:38 +0400

    Added HTTP request logger middleware

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express, { Application, Request, Response } from 'express';
+import morgan from 'morgan';
 import cors from 'cors';
 
 const app: Application = express();
 const port = 5000;
 
 app.use(cors());
+app.use(morgan('dev'));
 app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
