86698fc5a841c8644bbba8922e50a02e57e6110e
Giga Amiridze <gigaamiridze@gmail.com>
2023-08-26 23:08:17 +0400

    Added cors and json body parser middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
 
 const app: Application = express();
 const port = 5000;
+
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, World!');
