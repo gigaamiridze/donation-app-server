ef82018132c033a3c00bf600850ba2cd8fa2b587
Giga Amiridze <gigaamiridze@gmail.com>
2023-08-27 11:38:04 +0400

    Added payment functionality using stripe

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express, { Application, Request, Response } from 'express';
+import Stripe from 'stripe';
 import morgan from 'morgan';
 import cors from 'cors';
 import { PORT, STRIPE_PUBLISHABLE_KEY, STRIPE_SECRET_KEY } from './env';
@@ -9,8 +10,40 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, World!');
+app.post('/create-payment-intent', async (req: Request, res: Response) => {
+  const { email, currency, amount } = req.body;
+
+  if (STRIPE_SECRET_KEY) {
+    const stripe = new Stripe(STRIPE_SECRET_KEY, {
+      apiVersion: '2023-08-16',
+    });
+
+    const customer = await stripe.customers.create({ email });
+
+    const params: Stripe.PaymentIntentCreateParams = {
+      amount: parseInt(amount),
+      currency,
+      customer: customer.id,
+      payment_method_options: {
+        card: {
+          request_three_d_secure: 'automatic',
+        },
+      },
+      payment_method_types: ['card'],
+    }
+
+    try {
+      const paymentIntent = await stripe.paymentIntents.create(params);
+      return res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    } catch (error) {
+      const stripeError: Stripe.StripeRawError = error as Stripe.StripeRawError;
+      return res.send({
+        error: stripeError.message,
+      });
+    }
+  }
 });
 
 app.listen(PORT, () => {
